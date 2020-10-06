fd407aba8e5ea9badb8d6ebc7864dfe412ec54ba
AforSmithz <abiseno.arya@gmail.com>
2020-10-06 23:39:19 +0700

    add update and delete items

--- todoApp/views.py
+++ todoApp/views.py
@@ -18,3 +18,25 @@ def index(request):
     return render(request, 'tasks/list.html', context)
 
 
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = formtask(instance=task)
+
+    if request.method == 'POST':
+        form = formtask(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/') 
+
+    context = {'form' : form} 
+    return render(request, 'tasks/update.html', context)
+
+def deleteTask(request,pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item' :item}
+    return render(request, 'tasks/delete.html' ,context)
